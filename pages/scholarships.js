6d06e51a40fab141b1f9eb6eb209bb900569fc4d
D.Chirukesh <chirukeshdevabhathini@gmail.com>
2024-12-19 10:34:03 +0530

    name update

--- pages/scholarships.js
+++ pages/scholarships.js
@@ -36,7 +36,7 @@ const ScholarshipsPage = () => {
         <div>
             <Navbar />
             <div className="max-w-6xl mx-auto p-6">
-                <h1 className="text-3xl font-bold mb-6">Available Scholarships</h1>
+                <h1 className="text-3xl font-bold mb-6">Available Courses</h1>
 
                 {/* Scholarships Grid */}
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
